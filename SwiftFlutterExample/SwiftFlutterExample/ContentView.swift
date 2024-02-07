76cd04d331ef9e06239dd5d9b43fb1afcf1c902d
Ryan Weald <ryan@weald.com>
2024-02-07 12:18:49 -0800

    Add basic UI to test wallet CRUD through flutter channel

--- SwiftFlutterExample/SwiftFlutterExample/ContentView.swift
+++ SwiftFlutterExample/SwiftFlutterExample/ContentView.swift
@@ -5,27 +5,50 @@
 //  Created by Nicholas Bui LeTourneau on 2/6/24.
 //
 
-import SwiftUI
 import Flutter
+import SwiftUI
 
 struct ContentView: View {
     // Flutter dependencies are passed in an EnvironmentObject.
     @EnvironmentObject var flutterDependencies: FlutterDependencies
-    
+
     // Button is created to call the showFlutter function when pressed.
     var body: some View {
-        Button("Send method to Flutter!") {
-            sendToFlutter()
+        VStack {
+            Button("Send method to Flutter!") {
+                sendToFlutter()
+            }
+            .padding(.bottom, 24) // Add 24px of padding to the bottom of the first button
+
+            Button("Clear Wallet") {
+                clearWallet()
+            }
         }
     }
-    
+
     func sendToFlutter() {
         let channel = FlutterMethodChannel(name: "channels.rly.network/wallet_manager",
                                            binaryMessenger: flutterDependencies.flutterEngine.binaryMessenger)
-        
-        channel.invokeMethod("performSomeAction", arguments: nil) { (response) in
+
+        channel.invokeMethod("getWalletAddress", arguments: nil) { response in
             // Handle the Flutter method response if necessary
-            print("response", response!)
+            print("existing wallet = ", response!)
+
+            if response as! String == "no address" {
+                channel.invokeMethod("createWallet", arguments: nil) { response in
+                    print("successfully created wallet")
+                    print("New wallet address = ", response!)
+                }
+            }
+        }
+    }
+
+    func clearWallet() {
+        let channel = FlutterMethodChannel(name: "channels.rly.network/wallet_manager",
+                                           binaryMessenger: flutterDependencies.flutterEngine.binaryMessenger)
+
+        channel.invokeMethod("deleteWallet", arguments: nil) { _ in
+            print("cleared wallet")
         }
     }
 }
