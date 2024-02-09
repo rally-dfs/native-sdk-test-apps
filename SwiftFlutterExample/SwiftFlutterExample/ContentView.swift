fa28cdcda02e2c71a0e898e502e97f7b5f23141d
Ryan Weald <ryan@weald.com>
2024-02-09 14:41:33 -0800

    Put loading indicator over screen when performing on chain action

--- SwiftFlutterExample/SwiftFlutterExample/ContentView.swift
+++ SwiftFlutterExample/SwiftFlutterExample/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
 
     @State private var envSetup: Bool = false
     @State private var didBootstrap: Bool = false
+    @State private var performingAction: Bool = false
 
     private var channel: FlutterMethodChannel {
         FlutterMethodChannel(name: "channels.rly.network/wallet_manager",
@@ -25,41 +26,52 @@ struct ContentView: View {
 
     // Button is created to call the showFlutter function when pressed.
     var body: some View {
-        VStack {
-            if didBootstrap {
-                if walletAddress.isEmpty {
-                    Button("Create Wallet") {
-                        createWallet()
-                    }.padding(.bottom, 24)
+        ZStack {
+            VStack {
+                if didBootstrap {
+                    if walletAddress.isEmpty {
+                        Button("Create Wallet") {
+                            createWallet()
+                        }.padding(.bottom, 24)
+                    } else {
+                        Text("Wallet Address: \(walletAddress)").padding(.horizontal, 14)
+                        Button("Clear Wallet") {
+                            clearWallet()
+                        }.padding(.top, 24).padding(.bottom, 48)
+                    }
+
+                    if !walletAddress.isEmpty && !envSetup {
+                        Button("Setup Blockchain Env") {
+                            setupEnv()
+                        }.padding(.bottom, 24)
+                    }
+
+                    if envSetup {
+                        Button("Claim RLY Tokens") {
+                            claimRly()
+                        }.padding(.bottom, 24).padding(.top, 48)
+
+                        Button("Check RLY Token Balance") {
+                            getBalance()
+                        }.padding(.bottom, 24)
+
+                        Button("Transfer Some RLY Tokens") {
+                            transferRly()
+                        }.padding(.bottom, 24)
+                    }
+
                 } else {
-                    Text("Wallet Address: \(walletAddress)").padding(.horizontal, 14)
-                    Button("Clear Wallet") {
-                        clearWallet()
-                    }.padding(.top, 24).padding(.bottom, 48)
+                    Text("Attempting to Load Pre Existing Wallet")
                 }
+            }
 
-                if !walletAddress.isEmpty && !envSetup {
-                    Button("Setup Blockchain Env") {
-                        setupEnv()
-                    }.padding(.bottom, 24)
-                }
+            if performingAction {
+                Color.black.opacity(0.4)
+                    .edgesIgnoringSafeArea(.all)
 
-                if envSetup {
-                    Button("Claim RLY Tokens") {
-                        claimRly()
-                    }.padding(.bottom, 24).padding(.top, 48)
-
-                    Button("Check RLY Token Balance") {
-                        getBalance()
-                    }.padding(.bottom, 24)
-
-                    Button("Transfer Some RLY Tokens") {
-                        transferRly()
-                    }.padding(.bottom, 24)
-                }
-
-            } else {
-                Text("Attempting to Load Pre Existing Wallet")
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    .scaleEffect(2)
             }
         }.onAppear {
             getExistingWallet()
@@ -103,7 +115,9 @@ struct ContentView: View {
     }
 
     func claimRly() {
+        performingAction = true
         channel.invokeMethod("claimRly", arguments: nil) { response in
+            performingAction = false
             print("claimed RLY txn = ", response!)
         }
     }
@@ -111,19 +125,26 @@ struct ContentView: View {
     func transferTaki() {
         let takiTokenAddress = "0x8f5858b95797558f6facca3a9f20a081c3af6880"
         let recipientAddress = "0xc073ade46aba2f72bf27e7befd37af9301cd8920"
+
+        performingAction = true
         channel.invokeMethod("transferPermit", arguments: ["destinationAddress": recipientAddress, "amount": "2000000000000000000", "wrapperType": "Permit", "tokenAddress": takiTokenAddress]) { response in
+            performingAction = false
             print("transfer txn = ", response!)
         }
     }
 
     func transferRly() {
+        performingAction = true
         channel.invokeMethod("transferPermit", arguments: ["destinationAddress": "0xe75625f0c8659f18caf845eddae30f5c2a49cb00", "amount": "2000000000000000000"]) { response in
+            performingAction = false
             print("transfer txn = ", response!)
         }
     }
 
     func getBalance() {
+        performingAction = true
         channel.invokeMethod("getBalance", arguments: nil) { response in
+            performingAction = false
             print("balance = ", response!)
         }
     }
