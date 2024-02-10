2f5abcfeef7939daf5308df2d3a8197c97027832
Cameron Morris <ctmorris@gmail.com>
2024-02-09 16:47:53 -0800

    move sdk one level up

--- AndroidFlutterExample/settings.gradle.kts
+++ AndroidFlutterExample/settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri( "https://jitpack.io" )}
         maven {
-            url = uri("./rly_sdk")
+            url = uri("../rly_sdk")
         }
         maven {
             url= uri("https://storage.googleapis.com/download.flutter.io")
