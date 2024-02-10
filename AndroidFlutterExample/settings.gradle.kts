a8ab1cb4f7d339b5c5785c2df8f1e55a6d38ca4e
Cameron Morris <ctmorris@gmail.com>
2024-02-09 16:48:52 -0800

    updated path to sdk (#3)
    * updated path to sdk
    
    * move sdk one level up

--- AndroidFlutterExample/settings.gradle.kts
+++ AndroidFlutterExample/settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri( "https://jitpack.io" )}
         maven {
-            url = uri("/Users/cameron/rally/mobile/native_sdk_wrapper/build/host/outputs/repo")
+            url = uri("../rly_sdk")
         }
         maven {
             url= uri("https://storage.googleapis.com/download.flutter.io")
